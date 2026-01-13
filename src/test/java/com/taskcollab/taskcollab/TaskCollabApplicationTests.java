489b5cd45b8b64dfd535ea177a224edeafbaf272
Sadiq Ibrahim <yndaski@gmail.com>
2026-01-13 02:26:44 -0800

    first commit

---
+++ src/test/java/com/taskcollab/taskcollab/TaskCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taskcollab.taskcollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskCollabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
